51a906f934307d1421f8e10f467dc6dd715e3fe0
Guerline Jean-Baptiste <guerline.jean-baptiste@sonder.com>
2022-06-17 19:36:21 -0400

    #2

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -3,18 +3,15 @@ package com.pluralsight.blog.data;
 import com.pluralsight.blog.model.Author;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
- public interface PostRepository extends JpaRepository <Post, Long> {
+import java.util.stream.Collectors;
 
-  public default  List<Post> findByTitleContaining(String title){
-   List<Post> posts = findAll()
-           .stream()
-           .filter( p -> p.getTitle() != null && p.getTitle().contains(title))
-           .toList();
-   return posts;
-  }
+public interface PostRepository extends JpaRepository <Post, Long> {
+    @RestResource(rel="contains-title", path="containsTitle")
+    public List<Post> findByTitleContaining(String title);
 }
